d09dd2dc032de9d470a5214f2157088d6d7a41d9
Armine8 <armisha15@mail.ru>
2022-10-26 11:53:53 +0300

    Switch

--- src/main/java/school/mjc/stage0/conditions/task5/LeapYearPrinter.java
+++ src/main/java/school/mjc/stage0/conditions/task5/LeapYearPrinter.java
@@ -1,6 +1,14 @@
 package school.mjc.stage0.conditions.task5;
 
 public class LeapYearPrinter {
-    public void isLeapYear(int year) {
+    public static void isLeapYear(int year) {
+        if (year % 4 == 0){
+            System.out.println("leap");
+        } else {
+            System.out.println("not leap");
+        }
+    }
+    public static void main(String[] args){
+        isLeapYear(2001);
     }
 }
--- src/main/java/school/mjc/stage0/conditions/task5/SwitchDaysInMonthPrinter.java
+++ src/main/java/school/mjc/stage0/conditions/task5/SwitchDaysInMonthPrinter.java
@@ -1,6 +1,38 @@
 package school.mjc.stage0.conditions.task5;
 
 public class SwitchDaysInMonthPrinter {
-    public void amountOfDays(int month) {
+    public static void amountOfDays(int month) {
+        int days;
+
+        switch (month){
+            case (2):
+                days = 28;
+                System.out.println(days);
+                break;
+            case (4):
+            case (6):
+            case (9):
+            case (11):
+                days = 30;
+                System.out.println(days);
+                break;
+            case (1):
+            case (3):
+            case (5):
+            case (7):
+            case (8):
+            case (10):
+            case (12):
+                days = 31;
+                System.out.println(days);
+                break;
+            default:
+                System.out.println("wrong number!");
+                break;
+        }
+    }
+
+    public static void main(String[] args){
+        amountOfDays(11);
     }
 }
